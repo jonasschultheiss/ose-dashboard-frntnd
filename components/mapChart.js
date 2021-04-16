87ca5e8210b40b15336db940fff959f41593dec8
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2021-04-16 15:36:39 +0200

    :alembic: changed marker svg

--- components/mapChart.js
+++ components/mapChart.js
@@ -54,8 +54,17 @@ const MapChart = () => {
                 key={index}
                 coordinates={[model.location.lng, model.location.lat]}
               >
-                <div className="w-8 h-8 bg-pink-300"></div>
-                <circle r={4} fill="#F53" />
+                <g
+                  fill="none"
+                  stroke="#FF5533"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  transform="translate(-12, -24)"
+                >
+                  <circle cx="12" cy="10" r="3" />
+                  <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                </g>
               </Marker>
             );
           })}
