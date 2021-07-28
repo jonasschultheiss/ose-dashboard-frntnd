0e3751ade2190f95ad96db01939d30ba05771078
Robin Aegerter <robin.aegerter@endress.com>
2021-07-28 10:28:24 +0200

    Added high quality model with less space

--- components/scene.js
+++ components/scene.js
@@ -4,19 +4,26 @@ import { lazy, Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 
 const Model = lazy(() => import('@components/model'));
+
 const OrbitControls = dynamic(
   import('@react-three/drei').then(module => module.OrbitControls),
+  { ssr: false }
+);
+
+const Environment = dynamic(
+  import('@react-three/drei').then(module => module.Environment),
   { ssr: false }
 );
 
 export default function Scene({ assetSelected }) {
   return (
     <div className="absolute w-screen h-screen">
-      <Canvas colorManagement shadowMap camera={{ position: [2, 1.5, 1], fov: 70 }}>
+      <Canvas colorManagement camera={{ position: [2, 1.5, 1], fov: 70 }}>
         <Lights />
         <OrbitControls />
         <group>
           <Suspense fallback={''}>
+            <Environment files={'background.hdr'} />
             <Model assetSelected={assetSelected} position={[0, -1, 0.3]} />
           </Suspense>
         </group>
